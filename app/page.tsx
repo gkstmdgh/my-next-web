9e42e00328cc3ab0fd9f1c85da9740f5177f675c
gkstmdgh <gkstmdgh110@gmail.com>
2024-11-10 14:07:42 +0900

    gks

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 export default function Home() {
   return (
     <div>
-    바로 리부트 정상화
-    바로 리부트 정상화
-    바로 리부트 정상화
+      <MyButton name="한" nickname="승호" age={18} today={new Date()}/>
     </div>
   );
 }
- 
+
+// button 나는 버튼을 생성하는 JS함수
+type TParams = {
+  name: string
+  nickname: string
+  age: number
+  today: Date
+}
+
+// button버튼위에 뜰 문자
+// function MyButton(params: TParams) {
+//   return <div>I am {params.name} my name is {params.nickname} my age {params.age} time{params.today.getDate()}</div>
+// }
+
+function MyButton(params: TParams) {
+  return <div className="bg-red-500">  I am {params.name} my name is {params.nickname} time{params.today.getDate()}</div>
+}
+
